136e94cf891f77509d65b8b31bc409576e81875c
Artyom Shatunovsky <shtarty@gmail.com>
2016-07-04 15:04:51 +0300

    Lab 10.2

--- Lab_10/FileFinder/FileFinder/Program.cs
+++ Lab_10/FileFinder/FileFinder/Program.cs
@@ -18,19 +18,86 @@ namespace FileFinder
                 Console.WriteLine("Enter <directory> <search term> or -e for exit");
 
                 string input = Console.ReadLine();
-                if (input != null)
+                string[] commands;
+
+                try
                 {
-                    var commands = input.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
-                    if (commands.Length == 1 && commands[0] == "-e")
+                    commands = ParseAndCheckCommands(input);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("Directory not found");
+                    continue;
+                }
+                catch (InvalidCommandException)
+                {
+                    Console.WriteLine("Invalid command.");
+                    continue;
+                }
+
+                if (commands[0] == "-e")
+                {
+                    isExit = true;
+                    continue;
+                }
+
+                List<string> resulFiles = SearchForFiles(commands[0], commands[1]);
+
+                if (resulFiles.Count == 0)
+                {
+                    Console.WriteLine("No files found.");
+                }
+                else
+                {
+                    foreach (var file in resulFiles)
                     {
-                        isExit = true;
+                        Console.WriteLine(file);
                     }
-                    else if (commands.Length == 2)
-                    {
-                        
-                    }
-               }
+                    Console.WriteLine($"{resulFiles.Count} files found.");
+                }
+
             }
+        }
+
+        static List<string> SearchForFiles(string directory, string searchTerm)
+        {
+            List<string> resultFiles = new List<string>();
+            string[] subDirectories = Directory.GetDirectories(directory);
+            string[] files = Directory.GetFiles(directory);
+
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                if (fileName.ToLowerInvariant().Contains(searchTerm))
+                {
+                    resultFiles.Add(file);
+                }
+            }
+
+            foreach (var subDirectory in subDirectories)
+            {
+                Console.WriteLine($"Searching in {subDirectory}");
+                resultFiles.AddRange(SearchForFiles(subDirectory, searchTerm));
+            }
+
+            return resultFiles;
+        }
+
+        static string[] ParseAndCheckCommands(string text)
+        {
+            if (text != null)
+            {
+                var commands = text.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                if (commands.Length == 1 && commands[0] == "-e" || commands.Length == 2 && Directory.Exists(commands[0]))
+                {
+                    return commands;
+                }
+                if (!Directory.Exists(commands[0]))
+                {
+                    throw new DirectoryNotFoundException();
+                }
+            }
+            throw new InvalidCommandException();
         }
     }
 }
